34b9a6016f76846ad082cbc85578c92360c9ceec
abdennouhethjgg-cloud <abdennouhethjgg@gmail.com>
2026-09-15 19:57:32 +0200

    Update and rename EL2BCODE_Steal.lua to EL2BCODE_saeg.lua

--- EL2BCODE_Steal.lua
+++
@@ -1,4 +0,0 @@
--- EL2B legacy file retired.
--- This file is intentionally inactive and contains no gameplay automation.
--- Use EL2B_ALL_GEAR.lua for the safe interface-only version.
-warn("[EL2B] EL2BCODE_Steal.lua est désactivé. Utilisez EL2B_ALL_GEAR.lua.")
